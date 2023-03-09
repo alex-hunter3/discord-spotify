5b0f8e8ec56b51ffe2c93cc228693eced346227b
Alexander Hunter <alexander.hunter-4@student.manchester.ac.uk>
2023-03-09 08:53:27 +0000

    Add basic message handling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
-import { Client, ClientOptions } from "discord.js";
+import {
+  Client,
+  ClientOptions,
+  Events,
+  GatewayIntentBits,
+  Message,
+} from "discord.js";
 import { config } from "dotenv";
 
 config();
@@ -8,16 +14,41 @@ console.log("[STATUS] Starting bot...");
 class Bot extends Client {
   constructor(options: ClientOptions) {
     super(options);
+
+    this.setEventListeners();
+  }
+
+  private setEventListeners() {
+    this.once(Events.ClientReady, (client: Client) => {
+      console.log(`[STATUS] ${client.user?.username} is running!`);
+    });
+
+    this.on(Events.MessageCreate, this.handleMessage);
+  }
+
+  private handleMessage(msg: Message): void {
+    if (msg.author.bot || msg.author.id === this.user?.id) return;
+
+    console.log(
+      `[${msg.author.username}#${msg.author.discriminator}]: ${msg.content}`
+    );
   }
 
   async start() {
     await this.login(process.env.DISCORD_SECRET_KEY);
-    console.log("[STATUS] Bot started!");
+    console.log("[STATUS] Bot logged in!");
+    console.log(
+      `[STATUS] Invite bot using this link: ${process.env.DISCORD_BOT_INVITE_LINK}`
+    );
   }
 }
 
 const bot = new Bot({
-  intents: [],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 bot.start();
